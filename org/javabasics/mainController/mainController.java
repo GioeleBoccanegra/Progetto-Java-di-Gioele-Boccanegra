734e751968f8e840f35f699b35aa295778114a16
giobocca03 <gio.bocca03@gmail.com>
2025-06-16 16:50:20 +0200

    a

---
+++ org/javabasics/mainController/mainController.java
@@ -0,0 +1,14 @@
+package org.javabasics.mainController;
+
+import java.util.Scanner;
+
+public class mainController {
+  public static void menuInConsole() {
+    Scanner scan = new Scanner(System.in);
+
+
+    
+
+    scan.close();
+  }
+}
