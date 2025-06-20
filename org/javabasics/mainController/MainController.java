a0c30e423a20c5f43b1ea4ca2292a51eb646ed44
giobocca03 <gio.bocca03@gmail.com>
2025-06-20 16:34:50 +0200

    finito

--- org/javabasics/mainController/MainController.java
+++ org/javabasics/mainController/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
   public static void menuInConsole() {
     System.out.println("Digita 1: visualizare tutti gli obbiettivi nel sistema");
     System.out.println("Digita 2: decidere un obbiettivo esistente");
-    System.out.println("Digita 3: rimuovere un obbiettivo");
+    System.out.println("Digita 3: rimuovere una prenotazione");
     System.out.println("Digita 4: aggiungere nuovo utente");
     System.out.println("Digita 5: esporta file con obbiettivi disponibili");
     System.out.println("Digita 0: esci");
